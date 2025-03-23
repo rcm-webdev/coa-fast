8250d220ad489d38ebd043c8db56d952a2bf8eff
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-22 23:56:16 -0700

    create Hero, Footer, and Navbar components

---
+++ app/components/Hero.js
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Link from "next/link";
+import Reader from "@/public/ReadingDoodle.svg";
+import Glasses from "@/public/lucide--glasses.svg";
+
+function Hero() {
+  return (
+    <div className="hero bg-base-100 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <Image
+          src={Reader}
+          alt="Reader"
+          className="hidden md:block w-3/4"
+          loading="lazy"
+        />
+        <div className="space-y-6">
+          <div className="flex gap-3 items-center">
+            <h1 className="text-3xl font-bold">
+              Ophthalmic Technicians Roadmap
+            </h1>
+            <Image
+              src={Glasses}
+              alt="Glasses-icon"
+              className="bg-accent rounded-2xl p-2 w-10 h-10  md:w-14 md:h-14"
+            />
+          </div>
+          <div className="flex flex-col gap-6">
+            <p>
+              Follow the Ophthalmic Technicians roadmap to prepare your journey
+              at XYZ eye clinic. Follow the Ophthalmic Technicians roadmap to
+              prepare your journey at XYZ eye clinic.Follow the Ophthalmic
+              Technicians roadmap to prepare your journey at XYZ eye clinic.
+            </p>
+            <p className="badge badge-accent font-semibold ">Zero to COA.</p>
+          </div>
+          <div className="flex gap-2 justify-center md:justify-start">
+            <button className="btn btn-neutral w-full lg:w-1/3">
+              <Link href={"/"}>Start Learning</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
---
+++ app/components/Navbar.js
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/public/logo.svg";
+
+function Navbar() {
+  return (
+    <div className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <Link
+          href={"/"}
+          className="btn btn-ghost text-xl font-extrabold text-neutral"
+        >
+          <Image src={Logo} alt="Logo" className="h-10 w-10 " />
+          <span className="hidden md:block text-base-content">
+            Technicians Roadmap
+          </span>
+        </Link>
+      </div>
+      <div className="flex gap-2">{/* <Button /> */}</div>
+    </div>
+  );
+}
+
+export default Navbar;
