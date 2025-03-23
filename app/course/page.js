ebe66737706d9c97ef98013807840936466d0654
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-23 01:28:51 -0700

    start course homepage layout

--- app/course/page.js
+++ app/course/page.js
@@ -1,5 +1,19 @@
-function Course() {
-  return <div>Course Page</div>;
+import Navbar from "../components/Navbar";
+import { auth } from "@/auth";
+
+async function Course() {
+  const session = await auth();
+  return (
+    <div className="bg-base-200 min-h-screen">
+      <Navbar />
+      <div className="mx-auto max-w-5xl px-5 py-12 ">
+        <h1 className="font-extrabold text-4xl">
+          Welcome to Technicians Bootcamp, {session?.user?.name} 👋🏼
+        </h1>
+        <p></p>
+      </div>
+    </div>
+  );
 }
 
 export default Course;
