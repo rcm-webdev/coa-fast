adb348f3567d230025cb53afeccb72ef984343fc
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-23 23:16:36 -0700

    create course navbar

---
+++ app/components/CourseNavbar.js
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import Button from "./Button";
+
+function CourseNavbar() {
+  return (
+    <div className="sticky top-0 z-50 join bg-base-300 border-b flex items-center justify-between ">
+      <div className="flex items-center ">
+        <div className="join border-r ">
+          {/* Logo - always visible */}
+          <Link
+            href={"/course"}
+            className="join-item px-4 py-4 border-r hover:bg-base-200 duration-200"
+          >
+            <span className="font-bold">Technician&apos;s Bootcamp</span>
+          </Link>
+
+          {/* Progress Bar - hidden on small screens */}
+          <div className="join-item px-4 py-4 border-r hidden md:flex flex-col items-start">
+            <span className="text-sm text-bg-content">
+              Course Progress (100%)
+            </span>
+            <progress
+              className="progress w-32 mt-1"
+              value="100"
+              max="100"
+            ></progress>
+          </div>
+
+          {/* Current Section - hidden on small screens */}
+          <div className="join-item px-4 py-4 hidden text-sm md:flex md:flex-col md:gap-1">
+            <span>Current Section:</span>
+            <span className="link">Introduction</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Right side - user dropdown */}
+      <div className=" px-4 ">
+        <Button />
+      </div>
+    </div>
+  );
+}
+
+export default CourseNavbar;
