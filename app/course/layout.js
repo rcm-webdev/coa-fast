f5b283e56ceca91bc8d995c1c9d3ea848e465daa
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-23 23:17:39 -0700

    import course navbar and side bar to course layout

--- app/course/layout.js
+++ app/course/layout.js
@@ -1,5 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+import CourseNavbar from "../components/CourseNavbar";
+import CourseSidebar from "../components/CourseSidebar";
 
 async function LayoutPrivate({ children }) {
   const session = await auth();
@@ -8,7 +10,25 @@ async function LayoutPrivate({ children }) {
     redirect("/");
   }
 
-  return children;
+  return (
+    <div className="h-screen flex flex-col">
+      {/* Navbar - Fixed at the top */}
+      <div className="flex-shrink-0">
+        <CourseNavbar />
+      </div>
+
+      {/* Main Content - Sidebar and Scrollable Page */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar - Fixed on the left */}
+        <div className="flex-shrink-0">
+          <CourseSidebar />
+        </div>
+
+        {/* Scrollable Main Content */}
+        <div className="flex-1 overflow-y-auto bg-base-100">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default LayoutPrivate;
