6613c742df44fba51a87166f6de9f974f1096977
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-23 23:18:07 -0700

    update course width

--- app/course/page.js
+++ app/course/page.js
@@ -4,13 +4,15 @@ import { auth } from "@/auth";
 async function Course() {
   const session = await auth();
   return (
-    <div className="bg-base-200 min-h-screen">
-      <Navbar />
-      <div className="mx-auto max-w-5xl px-5 py-12 ">
+    <div className="bg-base-300 min-h-screen w-full overflow-y-auto">
+      <div className="mx-auto max-w-5xl text-center space-y-6 ">
         <h1 className="font-extrabold text-4xl">
           Welcome to Technicians Bootcamp, {session?.user?.name} 👋🏼
         </h1>
-        <p></p>
+        <p className="text-xl">
+          You&apos;re about to embark on a journey to learn the skills to be a
+          ophthalmic technician.{" "}
+        </p>
       </div>
     </div>
   );
