a4c715276a96d725ead6e8bcc1619a8d146e4627
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-03-26 20:58:29 -0700

    create roles for app

--- models/User.js
+++ models/User.js
@@ -1,61 +1,17 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true, // Ensure name is mandatory
-  },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    required: true,
-    unique: true, // Ensure email is unique
-  },
-  password: {
-    type: String,
-    required: true, // For authentication
-  },
-  role: {
-    type: String,
-    enum: ["student", "instructor", "admin"], // Define roles
-    default: "student",
-  },
-  image: {
-    type: String,
-  },
-
-  courses: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Course", // Reference to courses the user is enrolled in
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
     },
-  ],
-  progress: [
-    {
-      course: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Course",
-      },
-      completedLessons: {
-        type: Number,
-        default: 0,
-      },
-      totalLessons: {
-        type: Number,
-        default: 0,
-      },
-    },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
   },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
